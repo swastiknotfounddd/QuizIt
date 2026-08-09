3c1c17cd55d8cf5ba718208f5bd54c4d9bae2b5d
swastiknotfounddd <swastikjais12@gmail.com>
2026-08-09 21:51:33 +0530

    Apply black theme styling

---
+++ script.js
@@ -0,0 +1,32 @@
+const modal = document.getElementById('loginModal');
+
+function openLogin() {
+  modal.classList.add('show');
+}
+
+function closeLogin() {
+  modal.classList.remove('show');
+}
+
+function selectRole(selectedButton) {
+  document.querySelectorAll('.role button').forEach((button) => {
+    button.classList.remove('selected');
+  });
+
+  selectedButton.classList.add('selected');
+}
+
+function scrollToFeatures() {
+  document.getElementById('features').scrollIntoView({ behavior: 'smooth' });
+}
+
+function showDemoMessage() {
+  closeLogin();
+  alert('Bsdk bana to pahele.');
+}
+
+modal.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    closeLogin();
+  }
+});
